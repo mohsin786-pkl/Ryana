eaa739626a425546ed27d2f3fe598fe257662b0d
mohsin786-pkl <mohsin70862@gmail.com>
2025-07-03 14:06:38 +0500

    Add files via upload

---
+++ Index.php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP For Loop Example</title>
+</head>
+<body>
+    <h1>Numbers from 1 to 5 using PHP For Loop</h1>
+
+    <?php
+    for ($i = 1; $i <= 5; $i++) {
+        echo "Number: $i <br>";
+    }
+    ?>
+</body>
+</html>
